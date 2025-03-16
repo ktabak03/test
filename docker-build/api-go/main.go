25e5fdafd9822c28914032621b38b04fc5634aa2
ktabak03 <146417564+ktabak03@users.noreply.github.com>
2025-03-16 16:57:41 +0300

    create dir api-go

---
+++ docker-build/api-go/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
+
+// Структура для представления данных
+type Item struct {
+    ID    int               `json:"id"`
+    Order map[string]int    `json:"order"`
+}
+
+// Список элементов
+var items = []Item{
+    {
+        ID:    1,
+        Order: map[string]int{"banana": 100, "apple": 200},
+    },
+    {
+        ID:    2,
+        Order: map[string]int{"orange": 150, "grape": 50},
+    },
+}
+
+func main() {
+    // Создаем новый роутер
+    router := gin.Default()
+
+    // Определяем маршрут для получения списка
+    router.GET("/test", getItems)
+
+    // Запускаем сервер на порту 8080
+    router.Run(":3000")
+}
+
+// Хэндлер для получения списка
+func getItems(c *gin.Context) {
+    c.JSON(http.StatusOK, items)
+}
